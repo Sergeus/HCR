573e74ef77261f78892b520a32c859d2566e50f4
Chris Socha <cs2309@imperial.ac.uk>
2012-12-06 23:26:55 +0000

    Made retry a function. General refactoring

--- voice_recog/src/ros_speech_engine/src/ros_speech_engine.py
+++ voice_recog/src/ros_speech_engine/src/ros_speech_engine.py
@@ -92,58 +92,66 @@ class PocketSphinx:
         print data.data
         self.text = data.data
 
-
 def speak(sentence):
     call(["flite", "-t", sentence])
 
+def retry(attempt, success_state, fail_state, fail_text):
+    if attempt < 2 :
+        speak("I am sorry.  Could you say that again?")
+        return fail_state
+    else:
+        speak(fail_text) 
+        return success_state 
+
 def conversationStateMachine(ps, ros):
+   
+    state = "ASK_NAME"
+    attempt = 0
 
-    # If the service is started
-    if True:
+    random.seed()
+    iterator = 0 # iterates through different parts of a conversation
+    
+    # While we have user's attention
+    while (ros.checkStatus() != "STOP"):
+    
+        if state == "ASK_NAME":
 
-        state = "ASK_NAME"
-        
-        random.seed()
-        iterator = 0 # iterates through different parts of a conversation
-        
-        # While we have user's attention
-        while (ros.checkStatus() != "STOP"):
-        
-            if state == "ASK_NAME":
+            speak("Hello, what is your name?")
+            state = "RECOG_NAME"
 
-                speak("Hello, what is your name?")
-                state = "RECOG_NAME"
-
-            elif state == "RECOG_NAME":
+        elif state == "RECOG_NAME":
+           
+            name = Utterance(ps.listen()).getName()
+            
+            if name != None :
+                speak("Hello " + name + ".  My name is CHARLES.")
+            else :    
+                speak("Hello.  My name is CHARLES")
+            
+            state = "CHOOSE_STATE"
+            
+        elif state == "CHOOSE_STATE":
                
-                name = Utterance(ps.listen()).getName()
-                
-                if name != None :
-                    speak("Hello " + name + ".  My name is CHARLES.")
-                else :    
-                    speak("Hello.  My name is CHARLES")
-                
-                state = "CHOOSE_STATE"
-                
-            elif state == "CHOOSE_STATE":
-                   
 
-                if iterator == 0:
-                    state = "ASK_LOCATION"
-                elif iterator == 1:
-                    state = "ASK_CAKE"
-                else:
-                    state = "ASK_MEETING"
-                iterator = (iterator + 1) % 3
+            if iterator == 0:
+                state = "ASK_LOCATION"
+            elif iterator == 1:
+                state = "ASK_CAKE"
+            else:
+                state = "ASK_MEETING"
+            iterator = (iterator + 1) % 3
 
-            elif state == "ASK_LOCATION":
-                
-                speak("Where are you going to?")
-                state = "RECOG_LOCATION"
+        elif state == "ASK_LOCATION":
             
-            elif state == "RECOG_LOCATION":
-            
-                location = Utterance(ps.listen()).getLocation()
+            speak("Where are you going to?")
+            state = "RECOG_LOCATION"
+        
+        elif state == "RECOG_LOCATION":
+        
+            location = Utterance(ps.listen()).getLocation()
+            state = "ASK_INTERESTED"
+
+            if location != None:
                 if ("imperial" in location) or ("college" in location) or ("school" in location) or ("lectures" in location) or ("university" in location) :
                     speak("I can teach you everything there is to know.  A to Z. From Android to Robot.")
                 elif ("underground" in location) or ("tube" in location) or ("station" in location)  or ("line" in location):
@@ -156,74 +164,69 @@ def conversationStateMachine(ps, ros):
                     speak("Home is where the heart is.  If I only had heart.")
                 else :
                     speak("That sounds so very very exciting.  However, I can not travel up stairs.")
-                state = "ASK_INTERESTED"
+            else:
+                speak("That sounds so very very exciting.  However, I can not travel up stairs.")
+
+        elif state == "ASK_CAKE":
+            speak("Do you like cake?")
             
-            elif state == "ASK_CAKE":
-                speak("Do you like cake?")
-                state = "RECOG_CAKE"
-                attempt = 0
-                
-            elif state == "RECOG_CAKE":  
-                cake = Utterance(ps.listen())
-                state = "ASK_INTERESTED"
-                
-                if cake.containsYes() == True:
-                    speak("Unlucky. All the cake is gone.")
-                elif cake.containsNo() == True:
-                    speak("It must be wasted on you. I dream of cake.  And electric sheep")
-                else:
-                    attempt = attempt + 1
-                    if attempt < 2 :
-                        speak("I am sorry.  Could you say that again?")
-                        state = "RECOG_CAKE" 
-                    else:
-                        speak("How unfortunate.  Perhaps you are wiser than you first seemed.")    
-                
-            elif state == "ASK_MEETING":
-                
-                speak("Have you ever met a robot before?")
-                
-                state = "RECOG_MEETING"
-                attempt = 0
-                
-            elif state == "RECOG_MEETING":
+            state = "RECOG_CAKE"
+            attempt = 0
             
-                meeting = Utterance(ps.listen())
-                state = "ASK_INTERESTED"   
-                if meeting.containsYes() == True:
-                    speak("I think we might become best of friends sooner than I thought...")
-                elif meeting.containsNo() == True:
-                    speak("That is most unfortunate.  You have missed out...")
-                else:
-                    attempt = attempt + 1
-                    if attempt < 2 :
-                        speak("I am sorry.  Could you say that again?")
-                        state = "RECOG_MEETING"
-                    else:
-                        speak("Your words confuse me.")
-                             
-                            
-            elif state == "ASK_INTERESTED":
+        elif state == "RECOG_CAKE":  
+           
+            cake = Utterance(ps.listen())
+            state = "ASK_INTERESTED"
+            
+            if cake.containsYes() == True:
+                speak("Unlucky. All the cake is gone.")
+            elif cake.containsNo() == True:
+                speak("It must be wasted on you. I dream of cake.  And electric sheep")
+            else:
+                attempt = attempt + 1
+                state = retry(attempt, "ASK_INTERESTED", "RECOG_CAKE", "How unfortunate.  Perhaps you are wiser than you first seemed.")
+            
+        elif state == "ASK_MEETING":
+            
+            speak("Have you ever met a robot before?")
+            
+            state = "RECOG_MEETING"
+            attempt = 0
+            
+        elif state == "RECOG_MEETING":
+        
+            meeting = Utterance(ps.listen())
+            state = "ASK_INTERESTED"   
+            
+            if meeting.containsYes() == True:
+                speak("I think we might become best of friends sooner than I thought...")
+            elif meeting.containsNo() == True:
+                speak("That is most unfortunate.  You have missed out...")
+            else:
+                attempt = attempt + 1
+                state = retry(attempt, "ASK_INTERESTED", "RECOG_MEETING", "Your words confuse me");
+                        
+        elif state == "ASK_INTERESTED":
 
-                speak("Would you be interested in finding out more about this experiment?")
-                state = "RECOG_INTERESTED"
+            speak("Would you be interested in finding out more about this experiment?")
+            state = "RECOG_INTERESTED"
 
-            elif state == "RECOG_INTERESTED":
+        elif state == "RECOG_INTERESTED":
 
-                response = Utterance(ps.listen())
+            response = Utterance(ps.listen())
 
-                if response.containsYes()  == True:
-                    print "SUCCESS: Ticket printed"
-                elif response.containsNo() == True:
-                    print "UNLUCKY: Ticket not printed"
-                else:
-                    print "SUCCESS: Ticket printed"
-                
-                speak("It has been nice speaking to you.")
-                state = "ASK_NAME"
-            elif state == "ERROR":
-                print "Error state"
-                break
+            if response.containsYes()  == True:
+                print "SUCCESS: Ticket printed"
+            elif response.containsNo() == True:
+                print "UNLUCKY: Ticket not printed"
+            else:
+                print "SUCCESS: Ticket printed"
+            
+            speak("It has been nice speaking to you.")
+            state = "ASK_NAME"
+        elif state == "ERROR":
+            print "Error state"
+            break
         
 
 # Main functional loop
