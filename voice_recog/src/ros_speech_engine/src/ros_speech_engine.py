b275fccdc0665bcfb7df0d86d3ba765e34c13c76
Chris Tory <christory@hotmail.com>
2012-12-07 17:32:02 +0000

    Added arguement

--- voice_recog/src/ros_speech_engine/src/ros_speech_engine.py
+++ voice_recog/src/ros_speech_engine/src/ros_speech_engine.py
@@ -256,7 +256,7 @@ def conversationStateMachine(ps, ros):
                 speak("That is most unfortunate.  You have missed out...", "sad")
             else:
                 attempt = attempt + 1
-                state = retry(attempt, "ASK_INTERESTED", "RECOG_MEETING", "This will make the experiance more enoyable.  For me anyway.");
+                state = retry(attempt, "ASK_INTERESTED", "RECOG_MEETING", "This will make the experiance more enjoyable.  For me anyway.","happy");
                         
         elif state == "ASK_INTERESTED":
 
