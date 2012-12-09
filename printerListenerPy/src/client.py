a3a31d6e731884bb0e0e000a705d519d3f0bb362
Chris Socha <cs2309@imperial.ac.uk>
2012-12-09 19:01:12 +0000

    Added Python printer UDP client server (allows for printer to be run on a different machine).

---
+++ printerListenerPy/src/client.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import roslib; roslib.load_manifest('printerListenerPy')
+import rospy
+from messages.msg import printReceipt
+import socket
+import sys
+
+
+def callback(data):
+    HOST, PORT = "localhost", 9999
+    # SOCK_DGRAM is the socket type to use for UDP sockets
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    sock.sendto("PRINT-REQUEST", (HOST, PORT))
+
+
+def listener():
+    rospy.init_node('printerListenerPy', anonymous=True)
+    rospy.Subscriber("printerListenerPy", printReceipt , callback)
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    listener()
